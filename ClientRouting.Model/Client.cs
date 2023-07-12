615b51bc034caba1227eaa815542b0cb4a8298e2
John Thomas <John.Thomas+EYGS@ie.ey.com>
2023-07-12 17:33:32 +0100

    Base Project setup

---
+++ ClientRouting.Model/Client.cs
@@ -0,0 +1,40 @@
+﻿using BaseProject;
+
+namespace ClientRouting.Model
+{
+    public class Client :BaseObject
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }            
+        public List<ClientResource> Resources { get; set;}
+    }
+
+    public class ClientResource :BaseObject
+    {
+        public string Name { get; set; } 
+        public string Description { get; set; }
+        public ResourceType ResourceType { get; set; }
+        public List<ResourceConfiguration> ResourceConfigurations { get; set; }
+    }
+
+    public class ResourceType :BaseObject
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Code { get; set; }
+
+    }
+
+    public class ResourceConfiguration :BaseObject
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+
+    public class ConfigurationType : BaseObject
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Code { get; set; }
+    }
+}
